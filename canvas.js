dea3a77bcacb05053d7df125abf8f05f62c7556d
Jérémy PIEGAY <jeremy.piegay@live.fr>
2012-11-05 17:17:41 +0100

    Canvas

---
+++ canvas.js
@@ -0,0 +1,488 @@
+var tableauX_global = new Array();
+var tableauY_global = new Array();
+
+var tableauX_A = new Array();
+var tableauY_A = new Array();
+tableauX_A[0] = 64;
+tableauY_A[0] = 176;
+tableauX_A[1] = 79;
+tableauY_A[1] = 138;
+tableauX_A[2] = 115;
+tableauY_A[2] = 139;
+tableauX_A[3] = 154;
+tableauY_A[3] = 137;
+tableauX_A[4] = 167;
+tableauY_A[4] = 176;
+tableauX_A[5] = 131;
+tableauY_A[5] = 79;
+tableauX_A[6] = 100;
+tableauY_A[6] = 74;
+tableauX_global['A'] = tableauX_A;
+tableauY_global['A'] = tableauY_A;
+
+var tableauX_B = new Array();
+var tableauY_B = new Array();
+tableauX_B[0] = 75;
+tableauY_B[0] = 55;
+tableauX_B[1] = 151;
+tableauY_B[1] = 59;
+tableauX_B[2] = 117;
+tableauY_B[2] = 110;
+tableauX_B[3] = 159;
+tableauY_B[3] = 126;
+tableauX_B[4] = 150;
+tableauY_B[4] = 168;
+tableauX_B[5] = 75;
+tableauY_B[5] = 168;
+tableauX_B[6] = 75;
+tableauY_B[6] = 110;
+tableauX_global['B'] = tableauX_B;
+tableauY_global['B'] = tableauY_B;
+
+var tableauX_C = new Array();
+var tableauY_C = new Array();
+tableauX_C[0] = 161;
+tableauY_C[0] = 76;
+tableauX_C[1] = 148;
+tableauY_C[1] = 61;
+tableauX_C[2] = 101;
+tableauY_C[2] = 56;
+tableauX_C[3] = 69;
+tableauY_C[3] = 106;
+tableauX_C[4] = 79;
+tableauY_C[4] = 148;
+tableauX_C[5] = 113;
+tableauY_C[5] = 172;
+tableauX_C[6] = 160;
+tableauY_C[6] = 148;
+tableauX_global['C'] = tableauX_C;
+tableauY_global['C'] = tableauY_C;
+
+var tableauX_D = new Array();
+var tableauY_D = new Array();
+tableauX_D[0] = 74;
+tableauY_D[0] = 54;
+tableauX_D[1] = 133;
+tableauY_D[1] = 58;
+tableauX_D[2] = 165;
+tableauY_D[2] = 110;
+tableauX_D[3] = 149;
+tableauY_D[3] = 161;
+tableauX_D[4] = 74;
+tableauY_D[4] = 170;
+tableauX_D[5] = 74;
+tableauY_D[5] = 140;
+tableauX_D[6] = 75;
+tableauY_D[6] = 97;
+tableauX_global['D'] = tableauX_D;
+tableauY_global['D'] = tableauY_D;
+
+var tableauX_E = new Array();
+var tableauY_E = new Array();
+tableauX_E[0] = 162;
+tableauY_E[0] = 55;
+tableauX_E[1] = 81;
+tableauY_E[1] = 53;
+tableauX_E[2] = 82;
+tableauY_E[2] = 90;
+tableauX_E[3] = 82;
+tableauY_E[3] = 130;
+tableauX_E[4] = 80;
+tableauY_E[4] = 169;
+tableauX_E[5] = 159;
+tableauY_E[5] = 171;
+tableauX_E[6] = 154;
+tableauY_E[6] = 110;
+tableauX_global['E'] = tableauX_E;
+tableauY_global['E'] = tableauY_E;
+
+var tableauX_F = new Array();
+var tableauY_F = new Array();
+tableauX_F[0] = 157;
+tableauY_F[0] = 57;
+tableauX_F[1] = 121;
+tableauY_F[1] = 54;
+tableauX_F[2] = 84;
+tableauY_F[2] = 55;
+tableauX_F[3] = 86;
+tableauY_F[3] = 110;
+tableauX_F[4] = 142;
+tableauY_F[4] = 112;
+tableauX_F[5] = 86;
+tableauY_F[5] = 142;
+tableauX_F[6] = 87;
+tableauY_F[6] = 175;
+tableauX_global['F'] = tableauX_F;
+tableauY_global['F'] = tableauY_F;
+
+var tableauX_G = new Array();
+var tableauY_G = new Array();
+tableauX_G[0] = 163;
+tableauY_G[0] = 81;
+tableauX_G[1] = 103;
+tableauY_G[1] = 53;
+tableauX_G[2] = 63;
+tableauY_G[2] = 116;
+tableauX_G[3] = 98;
+tableauY_G[3] = 169;
+tableauX_G[4] = 159;
+tableauY_G[4] = 161;
+tableauX_G[5] = 164;
+tableauY_G[5] = 123;
+tableauX_G[6] = 128;
+tableauY_G[6] = 119;
+tableauX_global['G'] = tableauX_G;
+tableauY_global['G'] = tableauY_G;
+
+var tableauX_H = new Array();
+var tableauY_H = new Array();
+tableauX_H[0] = 160;
+tableauY_H[0] = 51;
+tableauX_H[1] = 161;
+tableauY_H[1] = 109;
+tableauX_H[2] = 160;
+tableauY_H[2] = 174;
+tableauX_H[3] = 117;
+tableauY_H[3] = 109;
+tableauX_H[4] = 73;
+tableauY_H[4] = 53;
+tableauX_H[5] = 74;
+tableauY_H[5] = 109;
+tableauX_H[6] = 73;
+tableauY_H[6] = 173;
+tableauX_global['H'] = tableauX_H;
+tableauY_global['H'] = tableauY_H;
+
+var tableauX_I = new Array();
+var tableauY_I = new Array();
+tableauX_I[0] = 116;
+tableauY_I[0] = 47;
+tableauX_I[1] = 117;
+tableauY_I[1] = 68;
+tableauX_I[2] = 116;
+tableauY_I[2] = 96;
+tableauX_I[3] = 117;
+tableauY_I[3] = 116;
+tableauX_I[4] = 117;
+tableauY_I[4] = 127;
+tableauX_I[5] = 117;
+tableauY_I[5] = 150;
+tableauX_I[6] = 116;
+tableauY_I[6] = 174;
+tableauX_global['I'] = tableauX_I;
+tableauY_global['I'] = tableauY_I;
+
+var tableauX_J = new Array();
+var tableauY_J = new Array();
+tableauX_J[0] = 141;
+tableauY_J[0] = 48;
+tableauX_J[1] = 141;
+tableauY_J[1] = 72;
+tableauX_J[2] = 141;
+tableauY_J[2] = 105;
+tableauX_J[3] = 140;
+tableauY_J[3] = 133;
+tableauX_J[4] = 135;
+tableauY_J[4] = 163;
+tableauX_J[5] = 96;
+tableauY_J[5] = 171;
+tableauX_J[6] = 79;
+tableauY_J[6] = 144;
+tableauX_global['J'] = tableauX_J;
+tableauY_global['J'] = tableauY_J;
+
+var tableauX_K = new Array();
+var tableauY_K = new Array();
+tableauX_K[0] = 74;
+tableauY_K[0] = 52;
+tableauX_K[1] = 75;
+tableauY_K[1] = 119;
+tableauX_K[2] = 74;
+tableauY_K[2] = 173;
+tableauX_K[3] = 153;
+tableauY_K[3] = 52;
+tableauX_K[4] = 114;
+tableauY_K[4] = 99;
+tableauX_K[5] = 143;
+tableauY_K[5] = 142;
+tableauX_K[6] = 160;
+tableauY_K[6] = 171;
+tableauX_global['K'] = tableauX_K;
+tableauY_global['K'] = tableauY_K;
+
+var tableauX_L = new Array();
+var tableauY_L = new Array();
+tableauX_L[0] = 86;
+tableauY_L[0] = 52;
+tableauX_L[1] = 85;
+tableauY_L[1] = 73;
+tableauX_L[2] = 86;
+tableauY_L[2] = 102;
+tableauX_L[3] = 87;
+tableauY_L[3] = 145;
+tableauX_L[4] = 89;
+tableauY_L[4] = 172;
+tableauX_L[5] = 123;
+tableauY_L[5] = 171;
+tableauX_L[6] = 162;
+tableauY_L[6] = 170;
+tableauX_global['L'] = tableauX_L;
+tableauY_global['L'] = tableauY_L;
+
+var tableauX_M = new Array();
+var tableauY_M = new Array();
+tableauX_M[0] = 64;
+tableauY_M[0] = 169;
+tableauX_M[1] = 64;
+tableauY_M[1] = 67;
+tableauX_M[2] = 97;
+tableauY_M[2] = 111;
+tableauX_M[3] = 116;
+tableauY_M[3] = 161;
+tableauX_M[4] = 138;
+tableauY_M[4] = 112;
+tableauX_M[5] = 173;
+tableauY_M[5] = 63;
+tableauX_M[6] = 173;
+tableauY_M[6] = 165;
+tableauX_global['M'] = tableauX_M;
+tableauY_global['M'] = tableauY_M;
+
+var tableauX_N = new Array();
+var tableauY_N = new Array();
+tableauX_N[0] = 77;
+tableauY_N[0] = 173;
+tableauX_N[1] = 74;
+tableauY_N[1] = 128;
+tableauX_N[2] = 73;
+tableauY_N[2] = 59;
+tableauX_N[3] = 113;
+tableauY_N[3] = 110;
+tableauX_N[4] = 151;
+tableauY_N[4] = 165;
+tableauX_N[5] = 158;
+tableauY_N[5] = 114;
+tableauX_N[6] = 158;
+tableauY_N[6] = 57;
+tableauX_global['N'] = tableauX_N;
+tableauY_global['N'] = tableauY_N;
+
+var tableauX_ = new Array();
+var tableauY_ = new Array();
+tableauX_[0] = 0;
+tableauY_[0] = 0;
+tableauX_[1] = 0;
+tableauY_[1] = 0;
+tableauX_[2] = 0;
+tableauY_[2] = 0;
+tableauX_[3] = 0;
+tableauY_[3] = 0;
+tableauX_[4] = 0;
+tableauY_[4] = 0;
+tableauX_[5] = 0;
+tableauY_[5] = 0;
+tableauX_[6] = 0;
+tableauY_[6] = 0;
+tableauX_global[''] = tableauX_;
+tableauY_global[''] = tableauY_;
+
+function random_letter()
+{
+	tabletter = new Array();
+	tabletter[0]="A";
+	tabletter[1]="B";
+	tabletter[2]="C";
+	tabletter[3]="D";
+	tabletter[4]="E";
+	tabletter[5]="F";
+	tabletter[6]="G";
+	tabletter[7]="H";
+	tabletter[8]="I";
+	tabletter[9]="J";
+	tabletter[10]="K";
+	tabletter[11]="L";
+	tabletter[12]="M";
+	tabletter[13]="N";
+	tabletter[14]="O";
+	tabletter[15]="P";
+	tabletter[16]="Q";
+	tabletter[17]="R";
+	tabletter[18]="S";
+	tabletter[19]="T";
+	tabletter[20]="U";
+	tabletter[21]="V";
+	tabletter[22]="W";
+	tabletter[23]="X";
+	tabletter[24]="Y";
+	tabletter[25]="Z";
+	/*document.getElementById("letter").innerHTML = tabletter[Math.floor ( Math.random() * 26 )];*/
+	document.getElementById("letter").innerHTML = tabletter[14];
+}
+
+// Keep everything in anonymous function, called on window load.
+if(window.addEventListener) {
+window.addEventListener('load', function () {
+  var canvas, context, canvaso, contexto;
+
+  // The active tool instance.
+  var tool;
+  var tool_default = 'pencil';
+  var compteur = 0;
+  var tableauX = new Array();
+  var tableauY = new Array();
+
+  function init () {
+    // Find the canvas element.
+    canvaso = document.getElementById('imageView');
+    if (!canvaso) {
+      alert('Error: I cannot find the canvas element!');
+      return;
+    }
+
+    if (!canvaso.getContext) {
+      alert('Error: no canvas.getContext!');
+      return;
+    }
+
+    // Get the 2D canvas context.
+    contexto = canvaso.getContext('2d');
+	
+    if (!contexto) {
+      alert('Error: failed to getContext!');
+      return;
+    }
+
+    // Add the temporary canvas.
+    var container = canvaso.parentNode;
+    canvas = document.createElement('canvas');
+    if (!canvas) {
+      alert('Error: I cannot create a new canvas element!');
+      return;
+    }
+
+    canvas.id     = 'imageTemp';
+    canvas.width  = canvaso.clientWidth;
+    canvas.height = canvaso.clientHeight;
+    container.appendChild(canvas);
+
+    context = canvas.getContext('2d');
+	context.strokeStyle='#578fba'; 
+	context.lineWidth=8;
+    // Activate the default tool.
+    if (tools[tool_default]) {
+      tool = new tools[tool_default]();
+    }
+
+    // Attach the mousedown, mousemove and mouseup event listeners.
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup',   ev_canvas, false);
+  }
+
+  // The general-purpose event handler. This function just determines the mouse 
+  // position relative to the canvas element.
+  function ev_canvas (ev) {
+    if (ev.layerX || ev.layerY == 0) { // Firefox
+      ev._x = ev.layerX;
+      ev._y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetY == 0) { // Opera
+      ev._x = ev.offsetX;
+      ev._y = ev.offsetY;
+    }
+
+    // Call the event handler of the tool.
+    var func = tool[ev.type];
+    if (func) {
+      func(ev);
+    }
+  }
+
+  // This function draws the #imageTemp canvas on top of #imageView, after which 
+  // #imageTemp is cleared. This function is called each time when the user 
+  // completes a drawing operation.
+  function img_update () {
+		/*Marge d'erreur sur la position des points*/
+		var marge = 9;
+		/*alert(document.getElementById('letter').innerHTML);*/
+		tableauX_letter = new Array();
+		tableauY_letter = new Array();
+		tableauX_letter = tableauX_global[document.getElementById('letter').innerHTML];
+		tableauY_letter = tableauY_global[document.getElementById('letter').innerHTML];
+		var flag;
+		var j = 0;
+		var i;
+		for(j=0;j<7;j++)
+		{
+			flag=1;
+			i=0;
+			for(i=0;i<compteur;i++)
+			{
+				if((tableauX_letter[j]-marge<=tableauX[i] && tableauX[i]<=tableauX_letter[j]+marge) && (tableauY_letter[j]-marge<=tableauY[i] && tableauY[i]<=tableauY_letter[j]+marge))
+				{
+					flag=0;
+				}
+			}
+			if(flag==1)
+			{
+				break;
+			}
+		}
+		alert(tableauX[0]+' - '+tableauY[0]);
+		tableauX = new Array();
+		tableauY = new Array();
+		compteur=0;
+		if(flag==1)
+		{
+			document.getElementById('letter').style.border='3px solid red';
+		}
+		else
+		{
+			document.getElementById('letter').style.border='3px solid green';
+		}
+		contexto.drawImage(canvas, 0, 0);
+		
+  }
+
+  // This object holds the implementation of each drawing tool.
+  var tools = {};
+
+  // The drawing pencil.
+  tools.pencil = function () {
+    var tool = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        tool.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only 
+    // draws if the tool.started state is set to true (when you are holding down 
+    // the mouse button).
+    this.mousemove = function (ev) {
+      if (tool.started) {
+	  	tableauX[compteur] = ev._x;
+		tableauY[compteur] = ev._y;
+		compteur++;
+        context.lineTo(ev._x, ev._y);
+        context.stroke();
+      }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+        img_update();
+      }
+    };
+  };
+
+  init();
+
+}, false); }
