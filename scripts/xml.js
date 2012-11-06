812ea220da34e8e6b041e672d30825d08aced8a5
Guillaume Renoult <guillaume.renoult@tpi24.cpe.fr>
2012-11-06 17:22:31 +0100

    changement de la gestion des donnés, gestion de l'id du jeu

--- scripts/xml.js
+++ scripts/xml.js
@@ -1,16 +1,21 @@
 function getValuesFromXml(tab,idJeu)
  { 	
+
+ 		
+
 		for (var i=0, l=localStorage.length; i<l; i++){
-        var key = localStorage.key(i);
-        var value = localStorage[key];
-        tab[i]=new Array(key,parseInt(value));
+        	var key = localStorage.key(i);
+        	var value = localStorage[key].split(',')[1];
+        if (localStorage[key].split(',')[0]==idJeu) 
+        	tab[i]=new Array(key,parseInt(value));
     }
 
   }
 
 function writeScoreToXml(idJeu,date,note){
 if (localStorage) {
-  	localStorage.setItem(date, note);
+ var val= [idJeu,parseInt(note)];
+  	localStorage.setItem(date, val);
 } else {
  
 }
