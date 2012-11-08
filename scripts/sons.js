84c8bdeca063e2c17c0574723c5ccbaed41ecc43
ghosterk <arthur@aryo.fr>
2012-11-08 15:40:28 +0100

    Utilisation de Cordova pr Android

--- scripts/sons.js
+++ scripts/sons.js
@@ -3,17 +3,26 @@ $(document).ready(function() {
 });
 
 function play(audio_id) {
-	var audio_elem = $('#'+audio_id);
-	if( audio_elem != null ) {
-		audio_elem[0].play();
+	if( !isAndroid()) {
+		var audio_elem = $('#'+audio_id);
+		if( audio_elem != null ) {
+			audio_elem[0].play();
+		}
 	}
+	else
+		playAndroid(audio_id);
 }
 
 function play_src(audio_id, nom_son) {
-	var audio_elem = $('#'+audio_id);
-	if( audio_elem != null ) {		
-		change_src_elem(audio_elem, nom_son)
-    	$(audio_elem)[0].play();
+	if( !isAndroid()) {
+		var audio_elem = $('#'+audio_id);
+		if( audio_elem != null ) {		
+			change_src_elem(audio_elem, nom_son)
+	    	$(audio_elem)[0].play();
+		}
+	}
+	else{ 
+		playAndroid(nom_son); alert("android");
 	}
 }
 
@@ -31,6 +40,10 @@ function generate_sources(nom_son) {
 	return '<source src="sons/'+ nom_son +'.mp3"></source><source src="sons/'+ nom_son +'.ogg"></source>';
 }
 
+function generate_android_sources(nom_son) {
+	return 'file:///android_asset/www/sons/'+ nom_son +'.mp3';
+}
+
 function pause(audio_id) {
 	var audio_elem = $('#'+audio_id);
 	if( audio_elem != null ) {
@@ -44,4 +57,30 @@ function stop( audio_id ) {
 		audio_elem[0].pause();
 		audio_elem[0].currentTime = 0;
 	}
+}
+
+function isAndroid() {
+	var ua = navigator.userAgent.toLowerCase();
+	var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+	if(isAndroid) {
+		return true;
+	}
+	return false;
+}
+
+// Audio player
+//
+var mediaTimer = null;
+var my_media=null;
+
+// Play Full Title
+function playAndroid(audio_id) {
+	audio_src = generate_android_sources(audio_id);
+
+    // Create Media object from src   
+    my_media = new Media(audio_src,function(){alert('ok')},function(e){alert('error'+e.toString())});           	
+    alert(my_media.src);
+
+    // Play Full Title
+    my_media.play();
 }
